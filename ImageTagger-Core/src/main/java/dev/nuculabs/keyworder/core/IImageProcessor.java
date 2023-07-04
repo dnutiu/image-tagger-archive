994026b7bc6109fba53856f6cd52dbcd0a07a53e
dnutiu <denis.nutiu@gmail.com>
2023-07-04 23:58:17 +0300

    first commit

---
+++ ImageTagger-Core/src/main/java/dev/nuculabs/keyworder/core/IImageProcessor.java
@@ -0,0 +1,24 @@
+package dev.nuculabs.keyworder.core;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public interface IImageProcessor {
+    /**
+     * Read image from path.
+     *
+     * @param path Path to image
+     * @return BufferedImage a buffered image.
+     * @throws IOException If image cannot be read
+     */
+    default BufferedImage readImage(String path) throws IOException {
+        File imageFile = new File(path);
+        return ImageIO.read(imageFile);
+    }
+
+    BufferedImage cropImage(BufferedImage bufferedImage);
+
+    float[][][][] processImage(String imagePath);
+}
