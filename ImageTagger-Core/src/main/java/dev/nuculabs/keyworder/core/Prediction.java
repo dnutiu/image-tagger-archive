15a73e898b2434aa7f9ec82b7790b42873534f48
dnutiu <denis.nutiu@gmail.com>
2023-07-05 20:42:22 +0300

    Fix pom.xml: make it work with Maven Reactor

--- ImageTagger-Core/src/main/java/dev/nuculabs/keyworder/core/Prediction.java
+++
@@ -1,9 +0,0 @@
-package dev.nuculabs.keyworder.core;
-
-/**
- * Prediction record.
- * @param label - The predicted label.
- * @param confidence - The confidence of the prediction.
- */
-public record Prediction(String label, float confidence) {
-}
